2d5b90d91b856fae5f1abf5f59213081ebf9b95f
Vraj Ramavat <vraj.ramavat@gmail.com>
2026-06-24 11:00:00 +0530

    feat: map potentiometer to delay timing (v1.2)

--- week1/led_blink/led_blink.ino
+++ week1/led_blink/led_blink.ino
@@ -1,14 +1,20 @@
+const int POT_PIN = A0;
 unsigned long blinkCount = 0;
 void setup() {
   pinMode(13, OUTPUT);
+  pinMode(POT_PIN, INPUT);
   Serial.begin(9600);
 }
 void loop() {
+  int potValue = analogRead(POT_PIN);
+  int delayMs = map(potValue, 0, 1023, 100, 2000);
   digitalWrite(13, HIGH);
-  delay(500);
+  delay(delayMs);
   digitalWrite(13, LOW);
-  delay(500);
+  delay(delayMs);
   blinkCount++;
   Serial.print("Blink count: ");
-  Serial.println(blinkCount);
+  Serial.print(blinkCount);
+  Serial.print(" | Delay: ");
+  Serial.println(delayMs);
 }
