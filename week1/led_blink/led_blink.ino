9aac460fa61e831851f2d67b9dc6ed939908cd1a
Vraj Ramavat <vraj.ramavat@gmail.com>
2026-06-24 15:45:00 +0530

    docs: add comment header block for blink sketch (v1.3)

--- week1/led_blink/led_blink.ino
+++ week1/led_blink/led_blink.ino
@@ -1,20 +1,39 @@
+/**
+ * @file led_blink.ino
+ * @author Vraj Ramavat
+ * @brief Potentiometer-controlled LED blink rate with serial logging.
+ * 
+ * Reads an analog potentiometer voltage on pin A0 and maps it
+ * to control the blinking rate of the pin 13 LED, logging results.
+ */
+
+const int LED_PIN = 13;
 const int POT_PIN = A0;
 unsigned long blinkCount = 0;
+
 void setup() {
-  pinMode(13, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
   pinMode(POT_PIN, INPUT);
   Serial.begin(9600);
+  Serial.println("System Initialized: Potentiometer LED Blink Controller");
 }
+
 void loop() {
   int potValue = analogRead(POT_PIN);
   int delayMs = map(potValue, 0, 1023, 100, 2000);
-  digitalWrite(13, HIGH);
+  
+  digitalWrite(LED_PIN, HIGH);
   delay(delayMs);
-  digitalWrite(13, LOW);
+  
+  digitalWrite(LED_PIN, LOW);
   delay(delayMs);
+  
   blinkCount++;
   Serial.print("Blink count: ");
   Serial.print(blinkCount);
+  Serial.print(" | Pot Raw: ");
+  Serial.print(potValue);
   Serial.print(" | Delay: ");
-  Serial.println(delayMs);
+  Serial.print(delayMs);
+  Serial.println(" ms");
 }
